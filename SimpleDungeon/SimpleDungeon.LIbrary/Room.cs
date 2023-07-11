3432ceaab6081b02c6c20f65990b1e5ae5be4b1c
Matthew J Brown <matthew@lokislayer.com>
2023-07-11 00:10:38 -0500

    Model for characteristics of a room.

---
+++ SimpleDungeon/SimpleDungeon.LIbrary/Room.cs
@@ -0,0 +1,9 @@
+﻿namespace SimpleDungeon.LIbrary;
+
+public class Room
+{
+    private int _roomId { get; set; }
+    private string? _name { get; set; }
+    private string? _description { get; set; }
+    private List<Directions> _exits;
+}
